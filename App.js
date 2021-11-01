7824c228f20bb3471eaafb70e1788c867038abac
MaximeMeh <44493799+MaximeMeh@users.noreply.github.com>
2021-11-01 18:01:41 +0100

    Initial Commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>YO!</Text>
+      <Text>Hello Word!</Text>
       <StatusBar style="auto" />
     </View>
   );
