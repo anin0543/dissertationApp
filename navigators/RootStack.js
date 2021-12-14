13bb543c13c381027828c1550667d9312959ca97
Anina Mahat <anin0543@edu.easj.dk>
2021-12-14 08:11:06 +0100

    SideBar update

--- navigators/RootStack.js
+++ navigators/RootStack.js
@@ -1,14 +1,14 @@
 import React, { useEffect, useState, useMemo } from "react";
 
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { Colors } from "./../components/styles";
 const { primary, tertiary } = Colors;
 
 // React Navigation
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 // screens
 import Login from "./../screens/Loginpage";
 import Home from "./../screens/Homepage";
@@ -17,56 +17,107 @@ import TestLogin from "./../screens/TestLogin";
 import TestHome from "./../screens/TestHome";
 import SearchVideo from "../screens/SearchVideopage";
 import Profile from "../screens/Profilepage";
+import Factory from "../components/factory";
+import OnePerson from "../components/onePerson";
+import OnTheGo from "../components/onTheGo";
+import MeetingRoom from "../components/meetingRoom";
+import Family from "../components/family";
+import FooterTabScreen from "../components/footer";
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
 
-
-function HomeTabScreen() {
+export const SideBar = () => {
   return (
-    <Tab.Navigator screenOptions={{
-      headerShown: false,
-    }}>
-      <Tab.Screen name="Home" component={Home}  options={{
-          tabBarLabel: 'Home',
+    <Drawer.Navigator>
+      <Drawer.Screen
+        name="Home"
+        component={HomeTabScreen}
+        options={{ drawerLabel: "Home" }}
+      />
+      <Drawer.Screen
+        name="Factory"
+        component={Factory}
+        options={{ drawerLabel: "Factory" }}
+      />
+      <Drawer.Screen
+        name="Family"
+        component={Family}
+        options={{ drawerLabel: "Family" }}
+      />
+      <Drawer.Screen
+        name="OnTheGo"
+        component={OnTheGo}
+        options={{ drawerLabel: "OnTheGo" }}
+      />
+      <Drawer.Screen
+        name="MeetingRoom"
+        component={MeetingRoom}
+        options={{ drawerLabel: "MeetingRoom" }}
+      />
+      <Drawer.Screen
+        name="OnePerson"
+        component={OnePerson}
+        options={{ drawerLabel: "OnePerson" }}
+      />
+    </Drawer.Navigator>
+  );
+};
+
+export const HomeTabScreen = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: "Home",
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="home" color={color} size={size} />
           ),
-        }} />
-      <Tab.Screen name="Videos" component={SearchVideo} options={{
-          tabBarLabel: 'Videos',
+        }}
+      />
+      <Tab.Screen
+        name="Videos"
+        component={SearchVideo}
+        options={{
+          tabBarLabel: "Videos",
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="magnify" color={color} size={size} />
           ),
-        }}/>
-      <Tab.Screen name="Profile" component={Profile} options={{
-          tabBarLabel: 'Profile',
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: "Profile",
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="account" color={color} size={size} />
           ),
-        }}/>
+        }}
+      />
     </Tab.Navigator>
   );
-}
-
-
+};
 
 const RootStack = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-        initialRouteName="Hometab"
-      >
-        <Stack.Screen name="Login" component={Login} />
-
-        <Stack.Screen name="Hometab" component={HomeTabScreen} />
-
-        <Stack.Screen name="TestHome" component={TestHome} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+      initialRouteName="Sidebar"
+    >
+      <Stack.Screen name="Sidebar" component={SideBar} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Hometab" component={HomeTabScreen} />
+    </Stack.Navigator>
   );
 };
 
